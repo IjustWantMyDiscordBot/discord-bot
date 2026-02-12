90cf2a15125c673083f0fc08ed4d72a266e53e1c
IjustWantMyDiscordBot <duolingowin4@proton.me>
2026-02-12 21:51:22 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot connecté en tant que ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
